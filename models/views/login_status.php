88a19657b11a6e9d10fe3550115e93c31f5355c6
YuChiChen113 <131358929+YuChiChen113@users.noreply.github.com>
2024-06-08 01:32:43 +0800

    Delete models directory

--- models/views/login_status.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-$time = $_SERVER['REQUEST_TIME'];
-$timeout_duration = 3600;
-if (!isset($_SESSION['login']) || ($time - $_SESSION['LAST_ACTIVITY']) > $timeout_duration) {
-  header("Location: /php_project_demo/views/login.php");
-  exit();
-}
-?>
